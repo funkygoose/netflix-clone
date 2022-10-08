a6d58e60a17a595528f14357d491ca8558c8a54e
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-07 23:38:12 -0700

    initial commit

---
+++ src/components/Row.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const Row = ({title, fetchURL}) => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+      axios.get(fetchURL).then((response) => {
+        setMovies(response.data.results)
+      })
+    }, [fetchURL])
+
+    console.log(movies)
+  return (
+    <>
+      <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+      <div className='relative flex items-center'>
+        <div id={'slider'}>
+          {movies.map((item, id) => (
+            <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+              i
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Row;
