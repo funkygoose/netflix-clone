1b47386c5f7df64ca8e5860639eaa20313eaa8c1
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-08 00:10:00 -0700

    initial commit

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,25 +1,25 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Movie from './Movie';
 
 const Row = ({title, fetchURL}) => {
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([]);
+    
 
     useEffect(() => {
       axios.get(fetchURL).then((response) => {
         setMovies(response.data.results)
       })
-    }, [fetchURL])
+    }, [fetchURL]);
 
-    console.log(movies)
+    console.log(movies);
   return (
     <>
       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
       <div className='relative flex items-center'>
         <div id={'slider'}>
           {movies.map((item, id) => (
-            <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-              i
-            </div>
+            <Movie key={id} item={item}/>
           ))}
         </div>
       </div>
