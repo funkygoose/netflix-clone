6a25097a6e477537b97111bf1ec033a6fd6cf38d
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-12 23:40:18 -0700

    initial commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -35,10 +35,10 @@ const Main = () => {
         <div className='absolute w-full top-[20%] p-4 md:p-8'>
           <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
           <div className='my-4'>
-            <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
+            <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5 cursor-not-allowed' >
               Play
               </button>
-            <button className='border text-white border-gray-300 py-2 px-5 ml-4'>
+            <button className='border text-white border-gray-300 py-2 px-5 ml-4 cursor-not-allowed'>
               Watch Later
               </button>
           </div>
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,8 +5,8 @@ import { FaHeart, FaRegHeart } from 'react-icons/fa'
 const Movie = ({item}) => {
   const [like, setLike] = useState(false)
   return (
-    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-      <img className='w-full h-auto block' 
+    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-not-allowed relative p-2'>
+      <img className='w-full h-auto block ' 
       src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} 
       alt={item.title} />
 
