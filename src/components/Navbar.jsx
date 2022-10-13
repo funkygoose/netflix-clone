95ef416d628abd930496767b432abbbf6d4d03bf
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-12 23:35:51 -0700

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
       <div>
         <button className='text-white pr-4 cursor-not-allowed'>Sign In</button>
-        <button className='bg-red-600 px-6 py-2 rounded cursor-pointer cursor-not-allowed text-white'>Sign Up</button>
+        <button className='bg-red-600 px-6 py-2 rounded cursor-not-allowed text-white'>Sign Up</button>
       </div>
     </div>
   )
