2b4e8280de45a7678dea39f0f4f9b3d05440e917
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-08-22 10:45:53 -0700

    initial commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -38,7 +38,7 @@ const Main = () => {
             <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5 cursor-not-allowed' >
               Play
               </button>
-            <button className='order text-white border-gray-300 py-2 px-5 ml-4 cursor-not-allowed'>
+            <button className='border text-white border-gray-300 py-2 px-5 ml-4 cursor-not-allowed'>
               Watch Later
               </button>
           </div>
