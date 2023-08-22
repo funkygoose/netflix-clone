07fe438ac214bc9822b2dff6acb25142dd5ae6e8
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-08-22 10:46:30 -0700

    initial commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -35,7 +35,7 @@ const Main = () => {
         <div className='absolute w-full top-[20%] p-4 md:p-8'>
           <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
           <div className='my-4'>
-            <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5 cursor-not-allowed' >
+            <button className='order bg-gray-300 text-black border-gray-300 py-2 px-5 cursor-not-allowed' >
               Play
               </button>
             <button className='border text-white border-gray-300 py-2 px-5 ml-4 cursor-not-allowed'>
