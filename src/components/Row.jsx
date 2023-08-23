e93d7973f3643f3b47a2f6800d995449ee48f9c8
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-08-23 11:31:34 -0700

    initial commit

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -13,7 +13,7 @@ const Row = ({title, fetchURL, rowID}) => {
       })
     }, [fetchURL]);
 
-    const lideLeft = () => {
+    const slideLeft = () => {
       let slider = document.getElementById('slider' + rowID);
       slider.scrollLeft = slider.scrollLeft - 500;
     }
